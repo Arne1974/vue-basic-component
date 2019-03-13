b5d6047fe6c8c2b43a2a438a95817f2fbf5beb96
Arne Siemens <arsiemens@gmail.com>
2019-03-13 09:47:57 +0100

    Watchers to Respond to Async Updates

--- script.js
+++ script.js
@@ -3,58 +3,26 @@ var card = new Vue({
   data: {
     title: 'Dinosaurs',
     input: '',
-    speciesUpdated: 0,
-    dinosUpdated: 0,
-    items: [
-      { text: "Velociraptor", quantity: 5 },
-      { text: "Triceratops", quantity: 3 },
-      { text: "Stegosaurus", quantity: 6 }
-    ]
-  },
-  filters: {
-    capitalize: function (value) {
-      if (!value) return '';
-      value = value.toString();
-      return value.charAt(0).toUpperCase() + value.slice(1);
-    },
-    undercase: function (value) {
-      if (!value) return '';
-      value = value.toString();
-      return value.toLowerCase();
-    },
-    url: function (value) {
-      if (!value) return '';
-      value = value.toString();
-      return 'https://de.wikipedia.org/wiki/' + value;
-    }
+    buttonText: 'Add Dinosaur',
+    items: ["Velociraptor", "Triceratops", "Stegosaurus"]
   },
   computed: {
-    totalDinos: function () {
-      this.dinosUpdated += 1;
-      var sum = 0;
-      var items = this.items;
-
-      for (var i in items) {
-        sum += items[i].quantity;
-      }
-
-      return sum;
-    },
-    totalSpecies: function () {
-      this.speciesUpdated += 1;
-      return this.items.length;
+    buttonDisabled: function(){
+      return this.input == "";
     }
+  },
+  watch: {
+    input: _.debounce(function () {
+      this.buttonText = this.input !== "" ? "Add " + this.input : "Add Dinosaur";
+    }, 250)
   },
   methods: {
     addItem: function (e) {
       e.preventDefault();
       if (!this.input) return;
 
-      this.items.push({ text: this.input, quantity: 1 });
+      this.items.push(this.input);
       this.input = '';
-    },
-    removeItem: function (item) {
-      this.items.splice(item, 1);
     }
   }
 });
