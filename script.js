44472fd1023af0b438b619581710303bc2b1cd8e
Arne Siemens <arsiemens@gmail.com>
2019-03-13 12:11:54 +0100

    Bind values to Input Fields

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 var card = new Vue({
   el: '#card',
   data: {
-    total: 0,
-    amount: null
+    checked: false,
+    selected: [],
+    chosenDino: '',
+    single: '',
+    message: '',
+    dinos: ["Triceratops", "Velociraptor", "Tyrannosaurus"],
+    periods: [
+      { name: "Triassic", value: 1 },
+      { name: "Jurassic", value: 2 },
+      { name: "Cretaceous", value: 3 }
+    ]
   },
   methods: {
-    addDinos: function(){
-      this.total += parseInt(this.amount);
+    addDinos: function () {
+      this.count += this.amount;
     }
   }
 });
